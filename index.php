9b23e1564cdbfa608ebaf4db736a2c1ad91405cd
rednaske1a <aleksander@apg.si>
2025-04-01 15:45:34 +0200

    bootystrap

--- index.php
+++ index.php
@@ -18,6 +18,7 @@ $user = $_SESSION['user'];
 
         <title>APG</title>
         <link rel="stylesheet" href="style.css">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     </head>
     <body>
         <h1>Dobrodošli, <?php echo htmlspecialchars($user['username']); ?>!</h1>
--- login.php
+++ login.php
@@ -32,6 +32,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         <meta charset="UTF-8">
         <title>Prijava</title>
         <link rel="stylesheet" href="style.css">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     </head>
     <body>
         <h1>Prijava</h1>
