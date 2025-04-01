51042e4334aab7b8de5b71eead4656f2b6ad6fdf
rednaske1a <aleksander@apg.si>
2025-04-01 16:08:38 +0200

    abcd

--- login.php
+++ login.php
@@ -55,5 +55,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
         <p>Še nimate računa? <a href="register.php" style="color:blue;">Ustvarite ga tukaj</a>.</p>
         
+
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
     </body>
 </html>
